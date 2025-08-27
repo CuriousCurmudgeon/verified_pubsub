cf412e8bd7ef166210fcb496e8ae7b6f16e42636
Brian Meeker <brian.meeker@getthru.io>
2025-08-27 12:26:41 -0400

    Generated a new mix project

---
+++ lib/verified_pubsub.ex
@@ -0,0 +1,18 @@
+defmodule VerifiedPubsub do
+  @moduledoc """
+  Documentation for `VerifiedPubsub`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> VerifiedPubsub.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/verified_pubsub_test.exs
@@ -0,0 +1,8 @@
+defmodule VerifiedPubsubTest do
+  use ExUnit.Case
+  doctest VerifiedPubsub
+
+  test "greets the world" do
+    assert VerifiedPubsub.hello() == :world
+  end
+end
