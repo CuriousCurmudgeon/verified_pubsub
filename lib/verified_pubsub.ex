0f06e385f6c08035815376ca8dbd66f77672b667
Brian Meeker <brian.meeker@getthru.io>
2025-08-27 12:57:56 -0400

    Started outlining an API for the macros

--- lib/verified_pubsub.ex
+++ lib/verified_pubsub.ex
@@ -1,18 +1,43 @@
 defmodule VerifiedPubsub do
   @moduledoc """
-  Documentation for `VerifiedPubsub`.
-  """
-
-  @doc """
-  Hello world.
+  Defines macros for verified PubSub. This provides compile-time support for PubSub
+  so you know that you are using valid topics and handling all possible messages on that topic.
 
   ## Examples
-
-      iex> VerifiedPubsub.hello()
-      :world
-
+  ```
+  topic "campaigns" do
+    message :created, %{id: :string, account_id: :string, name: String.t()}},
+    message :updated, %{id: :string, account_id: :string, name: String.t()}},
+    message :deleted, %{id: :string, account_id: :string}}
+  end
+  ```
   """
-  def hello do
-    :world
+
+  defmacro __using__(_opts) do
+    quote do
+      Module.register_attribute(__MODULE__, :topics, accumulate: true)
+
+      import unquote(__MODULE__), only: [topic: 1, message: 2]
+      @before_compile unquote(__MODULE__)
+    end
+  end
+
+  defmacro __before_compile__(env) do
+    compile(Module.get_attribute(env.module, :topics))
+  end
+
+  defmacro topic(name, fun) do
+    quote bind_quoted: [name: name, fun: fun] do
+      @topics {name, fun}
+    end
+  end
+
+  defmacro message(name, schema) do
+    quote bind_quoted: [name: name, schema: schema] do
+      @messages {name, schema}
+    end
+  end
+
+  def compile(_topics) do
   end
 end
